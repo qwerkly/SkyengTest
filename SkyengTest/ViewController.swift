d215c8cefa38055b3c0a677cc4994b43b160cffd
Ivan Babich <IBabich@mtsbank.ru>
2020-03-13 16:15:49 +0300

    Initial Commit

---
+++ SkyengTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SkyengTest
+//
+//  Created by Бабич Иван Юрьевич on 13.03.2020.
+//  Copyright © 2020 Babich Ivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
